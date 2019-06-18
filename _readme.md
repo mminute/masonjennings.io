c2a48c8ffeccefd5b69bf1e790c3b710c6491561
Mason Jennings <mjennings@pinterest.com>
2019-06-17 20:35:25 -0700

    Generate Navigation for each page

--- _readme.md
+++ _readme.md
@@ -2,3 +2,4 @@ TODO:
 
 Update languages section
 interests page
+Match navigation -> generate nav header for each page
--- generator/generatePages.rb
+++ generator/generatePages.rb
@@ -1,23 +1,33 @@
+require_relative './ContentGenerators/Navigation'
+
+# Directories >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 root_directory = '/Users/mjennings/code/masonjennings.io/'
 pages_directory = root_directory + 'generator/Pages/'
 wrapper_directory = pages_directory + 'Wrapper/'
 component_directory = wrapper_directory + 'components/'
+navigation_file = component_directory + 'Navigation.html'
 
+# Regex >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 main_contents_regex = /(\s*)\{\{\sMainContents\s\}\}/
-template_regex = /(\s*)\{\{\s(.*)\s\}\}/
+nav_regex = /(\s*)\{\{\sNavigation\s\}\}/
+template_regex = /(\s*)\{\{\s(.*)\s\}\}/ # matches things like: Javascript.html, Footer.html
 
+# Utils >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def getLeadingWhiteSpace(match)
     match.captures[0].length
 end
 
+# Main function >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 wrapper_file = File.read(wrapper_directory + 'PageWrapper.html')
 
+pages = Dir.entries(pages_directory).select{ |f| File.file?(f) && !f.start_with?('.') }
 
-Dir.entries(pages_directory).select{ |f| File.file? f }.each do |pageName|
+pages.each do |pageName|
     lines = []
 
     wrapper_file.each_line do |line|
         body_match = line.match(main_contents_regex)
+        navigation_match = line.match(nav_regex)
         template_match = line.match(template_regex)
 
         if body_match
@@ -25,6 +35,12 @@ Dir.entries(pages_directory).select{ |f| File.file? f }.each do |pageName|
             File.read(pages_directory + pageName).each_line do |body_contents_line|
                 lines.push(' ' * leading_whitespace + body_contents_line)
             end
+        elsif navigation_match
+            Navigation.new(
+                page_name: pageName,
+                leading_whitespace: getLeadingWhiteSpace(navigation_match),
+                file: navigation_file,
+            ).generate(lines)
         elsif template_match
             leading_whitespace = getLeadingWhiteSpace(template_match)
             template_filename = component_directory + template_match.captures[1]
